5d573e2868c0a973b139d949ea14ebaaced870a7
Jimmy Li <jli8239@bths.edu>
2025-11-08 18:09:47 +0000

    save

---
+++ U2T1 /comeandgo.java
@@ -0,0 +1,18 @@
+public class comeandgo {
+    public static void main(String[] args) {
+                int x = 10;
+                int y = x;
+        if (x > 5) {
+            if (x > 10) {
+                y--;
+            }
+        } else {
+            int z = x;
+            if (y >= z) {
+                z++;
+            } else {
+                z--;
+            }
+        }
+    }
+}
