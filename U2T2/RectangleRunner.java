e926ea6d041f7bfdf17b7834dff522af9afe1ebd
Jimmy Li <jli8239@bths.edu>
2025-11-14 18:29:54 +0000

    save

---
+++ U2T2/RectangleRunner.java
@@ -0,0 +1,10 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+        Rectangle rectA = new Rectangle(80, 100);
+        Rectangle rectB = new Rectangle(80, 100);
+        System.out.println((rectA == rectB));
+        System.out.println(rectA);
+        System.out.println(rectB);
+
+    }
+}
