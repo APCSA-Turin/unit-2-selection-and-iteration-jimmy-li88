7abb7bf7c384ec0e3df3199e525d27fcad476c0c
Jimmy Li <jli8239@bths.edu>
2026-01-28 19:00:19 +0000

    save

---
+++ U4T1/DogRunner.java
@@ -0,0 +1,16 @@
+public class DogRunner {
+    public static void main(String[] args) {
+        String[] myStrs = { "hi", "bye", "what", "purple" };
+
+        int arrLen = myStrs.length;
+
+        int strLen = myStrs[3].length();
+
+        System.out.println(arrLen + strLen);
+
+        String str = myStrs[2].substring(1, 3);
+
+        System.out.println(str);
+
+    }
+}
